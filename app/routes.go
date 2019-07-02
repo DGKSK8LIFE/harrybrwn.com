a9253f287f1e2348823d6ede99c4fb2bbde829a8
Harrison Brown <harrybrown98@gmail.com>
2019-07-02 11:24:17 -0700

    formatting

--- app/routes.go
+++ app/routes.go
@@ -1,62 +1,48 @@
 package app
 
 import (
-	"database/sql"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"time"
 
+	_ "harrybrown.com/pkg/env" // init the .env variables
 	"harrybrown.com/web"
 )
-
-var db *sql.DB
 
 func init() {
 	web.TemplateDir = "static/templates/"
 	web.BaseTemplates = []string{"/index.html", "/nav.html"} // included in all pages
-
-	// mysqlCred := fmt.Sprintf("%s:%s/%s",
-	// 	env.Get("MYSQL_USER"),
-	// 	env.Get("MYSQL_PASSWORD"),
-	// 	env.Get("DB_NAME"))
-
-	// var err error
-	// db, err = sql.Open(env.Get("DATABASE"), mysqlCred)
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
 }
 
-var (
-	// Routes is a list of all the app's routes
-	Routes = []web.Route{
-		&web.Page{
-			Title:     "Harry Brown",
-			Template:  "pages/home.html",
-			RoutePath: "/",
-			Data: struct{ Age string }{
-				Age: fmt.Sprintf("%d years", int((time.Since(bday).Hours()/24)/365)),
-			},
+// Routes is a list of all the app's routes
+var Routes = []web.Route{
+	&web.Page{
+		Title:     "Harry Brown",
+		Template:  "pages/home.html",
+		RoutePath: "/",
+		Data: struct{ Age string }{
+			Age: fmt.Sprintf("%d years", int((time.Since(bday).Hours()/24)/365)),
 		},
-		&web.Page{
-			Title:     "Freelancing",
-			Template:  "pages/freelance.html",
-			RoutePath: "/freelance",
-		},
-		&web.Page{
-			Title:     "Resume",
-			Template:  "pages/resume.html",
-			RoutePath: "/resume",
-			Data:      getResumeContent("./static/data/resume.json"),
-		},
-	}
+	},
+	&web.Page{
+		Title:     "Freelancing",
+		Template:  "pages/freelance.html",
+		RoutePath: "/freelance",
+	},
+	&web.Page{
+		Title:     "Resume",
+		Template:  "pages/resume.html",
+		RoutePath: "/resume",
+		Data:      getResume("./static/data/resume.json"),
+	},
+	web.NewRoute("/static/", NewFileServer("static")), // handle file server
+}
 
-	bday = time.Date(1998, time.August, 4, 4, 0, 0, 0, time.UTC)
-)
+var bday = time.Date(1998, time.August, 4, 4, 0, 0, 0, time.UTC)
 
-func getResumeContent(file string) *resumeContent {
+func getResume(file string) *resumeContent {
 	b, err := ioutil.ReadFile(file)
 	if err != nil {
 		log.Println(err)
