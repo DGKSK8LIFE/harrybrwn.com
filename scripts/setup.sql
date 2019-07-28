0879e5d75db41440e6ac5d31d8e4ab496255b063
Harrison Brown <harrybrown98@gmail.com>
2019-07-28 14:44:18 -0700

    tech expertise section

---
+++ scripts/setup.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE harrybrown_com;
+USE harrybrown_com;
+
+CREATE TABLE resume
+(
+	name VARCHAR(64),
+	date VARCHAR(64),
+	title VARCHAR(64),
+	content VARCHAR(128),
+	bullet_points VARCHAR(1024)
+);
