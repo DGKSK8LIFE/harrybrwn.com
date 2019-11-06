636af9b5524cde227cfbef225d16525e298dbc29
Harry Brown <harrybrown98@gmail.com>
2019-11-05 22:46:18 -0800

    moved some js to seperate file and added color transitions

--- static/js/home.js
+++ static/js/home.js
@@ -1,4 +1,35 @@
-(function () {
-	let nav = document.getElementById("nav");
-	nav.classList.add("fixed-top");
+function elem(name) {
+	let element = document.getElementById(name);
+	if (element == null) {
+		element = document.getElementsByClassName(name)[0];
+	}
+	// window[name] = element;
+
+	return {
+		addclass: function(clsname) {
+			element.classList.add(clsname);
+			return this;
+		},
+		removeclass: function(clsname) {
+			element.classList.remove(clsname);
+			return this;
+		},
+		mouseover: function(fn) {
+			// fn.this = this;
+			element.addEventListener('mouseover', fn);
+			return this;
+		}
+	};
+}
+
+(() => {
+	let nav = elem('nav').addclass('fixed-top');
+
+	this.onscroll = () => {
+		if (pageYOffset >= 200)
+			nav.addclass('bg-dark');
+		else
+			nav.removeclass('bg-dark');
+	}
+	// elem('arrow').mouseover(() => {});
 })();
