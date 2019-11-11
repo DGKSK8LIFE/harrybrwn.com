8562f053f568945c99a4e6591ff3bf859c6259f8
Harry Brown <harrybrown98@gmail.com>
2019-11-11 13:01:49 -0800

    added test for web.Page templating

--- pkg/web/page_test.go
+++ pkg/web/page_test.go
@@ -1,6 +1,9 @@
 package web
 
 import (
+	"html/template"
+	"net/http"
+	"net/http/httptest"
 	"strings"
 	"testing"
 )
@@ -30,5 +33,39 @@ func TestPageTemplatesCollection(t *testing.T) {
 		if !strings.HasPrefix(tmp, TemplateDir) {
 			t.Error("p.AddTemplateFile should have added the TemplateDir to the front of its input")
 		}
+	}
+}
+
+func TestPageTemplating(t *testing.T) {
+	var (
+		p = &Page{
+			Title:        "Test Page",
+			Template:     "",
+			RoutePath:    "/testing",
+			HotReload:    false,
+			baseTmplName: BaseTemplateName,
+		}
+		err error
+	)
+	p.blob, err = template.New(p.baseTmplName).Parse("<p>title: {{.Title}}</p>\n<p>path: {{.RoutePath}}</p>")
+	if err != nil {
+		t.Error("template parsing error")
+	}
+
+	req, err := http.NewRequest("GET", p.Path(), nil)
+	if err != nil {
+		t.Error("failed to make request:", err)
+	}
+	rr := httptest.NewRecorder()
+
+	p.Handler().ServeHTTP(rr, req)
+
+	if rr.Code != 200 {
+		t.Error("bad responce code")
+	}
+
+	exp := "<p>title: Test Page</p>\n<p>path: /testing</p>"
+	if rr.Body.String() != exp {
+		t.Error("got:", rr.Body.String(), "expedted:", exp)
 	}
 }
